8c2c2056578ff54e491e7b5dab1a4c12cb61fc9d
Catarina Ramalho <catarina.santos@ccc.ufcg.edu.br>
2022-10-05 23:11:05 -0300

    Implementação dos algoritmos iterativos de Estrutura de Dados para a análise da ordem de crescimento

--- AnaliseAlgoritmosIterativos.java
+++ AnaliseAlgoritmosIterativos.java
@@ -19,6 +19,37 @@ public class AnaliseAlgoritmosIterativos {
         char[] b = new char[] { '1', '2', '3', '4' };
         System.out.println(eh_palindromo(b));
 
+         /*
+         * Questão 03
+         */
+        int[] a = new int[] {1,13,0,4,5};
+        System.out.println(Arrays.toString(two_sum(a,5)));
+
+         /*
+         * Questão 04
+         */
+        int numero = 3;
+        System.out.println(eh_primo(numero));
+        numero = 15;
+        System.out.println(eh_primo(numero));
+
+        /*
+         * Questão 05
+         */
+        int[] lista = new int[] { 1, 13, 3, 4, 5, 1};
+        System.out.println(tem_repetido(lista));
+        lista = new int[] { 1, 13, 3, 4, 5};
+        System.out.println(tem_repetido(lista));
+
+        /*
+         * Questão 06
+         */
+        lista = new int[] {4,5,6,1};
+        System.out.println(vetorCircular(lista,6));
+        lista = new int[] { 3,4,2};
+        System.out.println(vetorCircular(lista,8));
+
+
     }
 
     /*
@@ -65,7 +96,7 @@ public class AnaliseAlgoritmosIterativos {
      * 
      */
 
-    public int[] two_sum(int[] v, int target) {
+    public static int[] two_sum(int[] v, int target) {
         for (int i = 0; i < v.length; i++) {
             for (int j = i + 1; j < v.length; j++) {
                 if (v[i] + v[j] == target) {
@@ -75,5 +106,56 @@ public class AnaliseAlgoritmosIterativos {
         }
         return new int[0];
     }
+
+    /*
+     * Questão 04 - Escreva um método que verifica se um número é primo ou não.
+     * Ordem de Cresciemnto - f(n) = n
+     * 
+     */
+
+    public static boolean eh_primo(int n){
+        for (int i = 2; i < n; i++) {
+            if (n%i==0){
+                return false;
+            }
+        }return true;
+    }
+
+    /*
+     * Questão 05- Escreva um método que receba um array de inteiros não ordenado e verifica existe algum elemento repetido presente no array. Retorne true ou false.
+     * Ordem de CRescimento = f(n) = n**2
+     */
+
+    public static boolean tem_repetido(int[] v){
+        for (int i = 0; i < v.length; i++) {
+            for (int j = i + 1; j < v.length; j++) {
+                if (v[i] == v[j]) {
+                    return true;
+                }
+            }
+        } return false;
+    }
+
+    /*
+     * Questão 06- Escreva um método que receba um array inteiros e um inteiro N e retorne uma String que representa a impressão do array de forma circular. Ou seja, quando o último elemento for visitado ele deve voltar para o primeiro e continuar até chegar a N elementos impressos.
+     * Ordem de crescimento = f(n) = n**2
+
+     */
+    public static String vetorCircular(int[] array, int quantidadeElementos){
+        String out =  "";
+        int quociente = quantidadeElementos/array.length;
+        int resto =  quantidadeElementos%array.length;
+        for (int i = 0; i < quociente; i++) {
+            for (int j = 0; j < array.length; j++) {
+                out += array[j] + " ";
+            }
+            
+        }
+        for (int a = 0; a < resto; a++){
+            out += array[a] + " ";
+        }
+        return out.trim();
+    }
+    
 
 }
